5e914a097c4354c837af22d1b05b2245922592a7
Bishwas Koirala <wasubisu69@gmail.com>
2024-05-07 23:49:18 +0900

    created up to price id

--- app/api/payment/create/route.ts
+++ app/api/payment/create/route.ts
@@ -1,15 +1,58 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+// dummy form body
+const body = {
+  name: "Bishwas",
+  email: "koiralabishwas257@gmail.com",
+  phone: "080-3511-8306",
+  address: {
+    country: "jp",
+    postal_code: "244-0805",
+    city: "神奈川県",
+    line1: "横浜市戸塚区川上町",
+    line2: "川上団地6棟203",
+  },
+  product_id : "prod_Q2GxSqbpfzdba4",
+  price : 9999
+};
 
-export async function POST(req:NextRequest , res : NextResponse) {
-  
-  const customer = await stripe.customers.create({
-    name : "Bishwas" , 
-    email : "koiralabishwas257@gmail.com",
-    address : { country : "jp" , city : "神奈川県" , }
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export async function POST(req: NextRequest, res: NextResponse) {
+
+  // first need to await body
+  const newCustomer = await stripe.customers.create({
+    name : body.name,
+    email : body.email,
+    phone : body.phone,
+    address : {
+      country : body.address.country,
+      postal_code : body.address.postal_code,
+      city : body.address.city,
+      line1 : body.address.line1,
+      line2 : body.address.line2
+    }
+  });
+  const createdCustomer = await stripe.customers.retrieve(newCustomer.id);
+
+  // save the created customer id for later use
+  const customerID = createdCustomer.id
+
+
+  // create price of the selected product
+  const donationAmount =  await stripe.prices.create({
+    product : body.product_id,
+    currency : 'jpy',
+    unit_amount : body.price,
   })
 
-  return NextResponse.json(customer)
+  const createdAmount = await stripe.prices.retrieve(donationAmount.id)
+  
+  // save the created priceID here
+  const priceID = createdAmount.id
+
+
+
+  return NextResponse.json({ newCustomer, createdCustomer , customerID , donationAmount , createdAmount , priceID });
 }
