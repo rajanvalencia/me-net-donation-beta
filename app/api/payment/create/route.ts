13eeaf2a67d350000c6c19478ad5746e5b1a8edc
Bishwas Koirala <wasubisu69@gmail.com>
2024-05-09 20:34:33 +0900

    prototype endpoint commit
    returns payment intent and client secret

--- app/api/payment/create/route.ts
+++ app/api/payment/create/route.ts
@@ -3,88 +3,64 @@ import Stripe from "stripe";
 
 // dummy form body
 const body = {
-  name: "Bishwas",
-  email: "koiralabishwas257@gmail.com",
-  phone: "080-3511-8306",
-  address: {
-    country: "jp",
-    postal_code: "244-0805",
-    city: "神奈川県",
-    line1: "横浜市戸塚区川上町",
-    line2: "川上団地6棟203",
+  customer : {
+    name: "Bishwas",
+    email: "koiralabishwas257@gmail.com",
+    phone: "080-3511-8306",
+    address: {
+      country: "jp",
+      postal_code: "244-0805",
+      city: "神奈川県",
+      line1: "横浜市戸塚区川上町",
+      line2: "川上団地6棟203",
+    }
   },
-  product_id : "prod_Q2GxSqbpfzdba4",
-  price : 9999
+  product_id: "prod_Q2GxSqbpfzdba4",
+  price: 9999,
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST(req: NextRequest, res: NextResponse) {
+  
+  const {customer , product_id , price} = body
+  
 
-  // first need to await body
-  const newCustomer = await stripe.customers.create({
-    name : body.name,
-    email : body.email,
-    phone : body.phone,
-    address : {
-      country : body.address.country,
-      postal_code : body.address.postal_code,
-      city : body.address.city,
-      line1 : body.address.line1,
-      line2 : body.address.line2
-    }
-  });
-  const createdCustomer = await stripe.customers.retrieve(newCustomer.id);
-
-  // save the created customer id for later use
-  const customerID = createdCustomer.id
-
+  
+  const { id: customerId } = await stripe.customers.create(customer);
 
   // create price of the selected product
-  const donationAmount =  await stripe.prices.create({
-    product : body.product_id,
-    currency : 'jpy',
-    custom_unit_amount : {
-      enabled : true
-    },
-    unit_amount : body.price,
-  })
+  const { id: priceId } = await stripe.prices.create({
+    product: product_id,
+    currency: "jpy",
+    unit_amount: price,
+  });
 
-  const createdPrice = await stripe.prices.retrieve(donationAmount.id)
-  
-  // save the created priceID here
-  const priceID = createdPrice.id
+  // create an empty invoice
+  const { id: invoiceId } = await stripe.invoices.create({
+    customer: customerId,
+  });
 
-  const session = await stripe.checkout.sessions.create({
-    cancel_url : "www.yahoo.com",
-    line_items : [
-      {
-        price : priceID,
-        quantity : 1
-      },
+  // invoice id が　必要
+  await stripe.invoiceItems.create({
+    customer: customerId,
+    price: priceId,
+    invoice: invoiceId,
+  });
 
-    ],
-    mode : "payment",
-    success_url : "www.google.com"
-  })
-
-  //
-  const creteInvoiceItems = stripe.invoiceItems.create({
-    customer : customerID,
-    price : priceID
-  })
-  
-  // create invoice 
-  const createInvoice = stripe.invoices.create({
-    customer : customerID,
-    pending_invoice_items_behavior : "include",
-  })
+  const { payment_intent : paymentIntentId} = await stripe.invoices.finalizeInvoice(invoiceId)
 
 
-  
+  let response : Record<string , string | null>;
+
+  let clientSecret : string | null;
+  if (typeof  paymentIntentId === "string"){
+    const { client_secret } = await stripe.paymentIntents.retrieve(paymentIntentId)
+     response = {client_secret , paymentIntentId}
+  }
+
+    
 
 
-
-
-  return NextResponse.json({ newCustomer, createdCustomer , customerID , donationAmount , createdPrice , priceID , createInvoice });
+  return NextResponse.json(response);
 }
