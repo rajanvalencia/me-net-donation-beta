1764d4146aeb515f5eadc2d66f4036f68b441e29
Bishwas Koirala <wasubisu69@gmail.com>
2024-05-11 00:14:33 +0900

    will hold creating   /api/payment/confirm
    do some front end first

--- app/api/payment/confirm/route.ts
+++ app/api/payment/confirm/route.ts
@@ -1,14 +1,22 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
 import { z } from "zod";
+
+interface Body{
+  paymentIntentId : string,
+  paymentMethodId : string
+}
 
 const schema = z.object({
   paymentIntentId : z.string(),
   
 })
 
+// stripe instance is initialized with your secret key elsewhere
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string , {apiVersion : "2024-04-10"});
+export async function POST(req:NextRequest ) {
+  const body : Body = await req.json()
 
-
-export async function POST(req:NextRequest) {
   // paymentMethod , paymentIntentId を受け取る。
   // stripe api に問い合わせ, payment Intent を確定
   // 成功　または　失敗のレスポンス
@@ -18,5 +26,7 @@ export async function POST(req:NextRequest) {
   // 寄付控除証明証を　生成し　、関係者(事務局)に送信
 
   // 領収証を寄付者にメールにて送る！
+
+  return NextResponse.json(body)
 
 }
