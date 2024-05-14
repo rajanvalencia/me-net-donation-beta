1baa4aa13d7b3687f7883e15cc70339232d1e5a6
Bishwas Koirala <wasubisu69@gmail.com>
2024-05-14 23:43:06 +0900

    refatored into small components

---
+++ app/components/PaymentPage.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+interface Props {
+  clientSecret : string
+  paymentIntentId? : string
+}
+
+// must call load stripe outside component to avoid rerender
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
+const PaymentPage = ({clientSecret , paymentIntentId} : Props) => {
+  // let clientSecret =
+  //   "pi_3PF72RDSydiWZpHQ2HQcZO9t_secret_yEGSCaVJx5yynk3csId8YQcDo";
+  // let paymentnIntentId = "pi_3PF72RDSydiWZpHQ2HQcZO9t";
+  const options = {
+    layout: {
+      type: "tabs",
+      appearance: {
+        theme: "night",
+      },
+    },
+    clientSecret: clientSecret,
+    paymentnIntentId: paymentIntentId,
+  };
+
+  return (
+    <div className="bg-white">
+    <div>
+      <p>custom checkout provider</p>
+      {/* <div>
+        <CustomCheckoutProvider stripe={stripePromise} options={options}>
+          <CustomCheckoutForm/>
+        </CustomCheckoutProvider>
+      </div> */}
+    </div>
+    <Elements stripe={stripePromise} options={{clientSecret : clientSecret }}>
+      
+      <CheckoutForm/>
+    </Elements>
+  </div>
+  );
+};
+
+export default PaymentPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,11 @@
-'use client'
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "./components/CheckoutForm";
-import { CustomCheckoutProvider } from "@stripe/react-stripe-js";
-import CustomCheckoutForm from "./components/CustomCheckoutForm";
-
-
-// must call load stripe outside component to avoid rerender
-const stripePromise  = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+import PaymentPage from "./components/PaymentPage";
 
 export default function Home() {
 
-
-
-  // hard code datas for now
-  let clientSecret = "pi_3PF72RDSydiWZpHQ2HQcZO9t_secret_yEGSCaVJx5yynk3csId8YQcDo"
-  let paymentnIntentId = "pi_3PF72RDSydiWZpHQ2HQcZO9t"
-  const options   = {
-    layout :   {
-      type : 'tabs',
-      appearance : {
-        theme : "night"
-      }
-    },
-    clientSecret : clientSecret,
-    paymentnIntentId : paymentnIntentId
-  }
-
+  
   return (
-  <div className="bg-white">
     <div>
-      <p>custom checkout provider</p>
-      {/* <div>
-        <CustomCheckoutProvider stripe={stripePromise} options={options}>
-          <CustomCheckoutForm/>
-        </CustomCheckoutProvider>
-      </div> */}
+      <PaymentPage clientSecret="pi_3PGMOCDSydiWZpHQ2YkMUz4R_secret_dnRzmUL78WvLpBrPwrzdbCmQe"/>
     </div>
-    <Elements stripe={stripePromise} options={{clientSecret : clientSecret }}>
-      
-      <CheckoutForm/>
-    </Elements>
-  </div>
   );
 }
