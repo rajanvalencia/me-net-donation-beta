afe7b689e22b98914842399cc2ae40c08f5fbeda
Bishwash Koirala <wasubisu69@gmail.com>
2024-06-01 23:31:01 +0900

    using react query in fetch products

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 import React from 'react';
-import { useFetch } from '../hooks/useFetch';
+import { useQuery } from '@tanstack/react-query';
 
 interface ProductData {
   id: string;
@@ -13,22 +13,36 @@ interface ProductListProps {
   onProductSelect: (productId: string) => void;
 }
 
-const ProductList: React.FC<ProductListProps> = ({ onProductSelect }) => {
-  const { data, isLoading, error } = useFetch<ProductData>('/api/products');
-  console.log(data);
+const fetchProducts = async (): Promise<ProductData[]> => {
+  const response = await fetch('/api/products');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
 
-  const handleProductSelect = (productId: string) => {
-    onProductSelect(productId);
-  };
+const ProductList: React.FC<ProductListProps> = ({ onProductSelect }) => {
+  const { data , isLoading, error } = useQuery<ProductData[]>({
+    queryKey: ['products'],
+    queryFn:() => fetchProducts(),
+  });
+
+  if (isLoading) {
+    return <div className='text-white text-5xl loading-spinner'></div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <div>
       <p className="menu-title text-2xl text-white">Products</p>
-      {data.map((product) => (
+      {data &&  data.map((product) => (
         <button
           className="btn p-4 m-4 text-gray-200"
           key={product.id}
-          onClick={() => handleProductSelect(product.id)}
+          onClick={() => onProductSelect(product.id)}
         >
           {product.name}
         </button>
