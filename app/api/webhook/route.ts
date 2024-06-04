19ed302681f7049e966d320c436dba666630f311
Bishwas KOirala <wasubisu69@gmail.com>
2024-06-04 20:04:14 +0900

    webhook not working as expected

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,56 +1,72 @@
-import {stripe} from "@/app/utils/stripe"
+import { stripe } from "@/app/utils/stripe";
 
-const stripeSecret = stripe
+const stripeSecret = stripe;
 
-import { NextResponse , NextRequest } from "next/server"
+import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(req:NextRequest) {
-  const payload = await req.text()
-  const response = JSON.parse(payload)
+export async function POST(req: NextRequest) {
+  const payload = await req.text();
+  const response = JSON.parse(payload);
 
-  const sig = req.headers.get("Stripe-Signature")
+  const sig = req.headers.get("Stripe-Signature");
 
-  const dateTime = new Date(response?.created * 1000).toLocaleDateString()
-  const timeString = new Date(response?.created * 1000).toLocaleDateString()
+  const dateTime = new Date(response?.created * 1000).toLocaleDateString();
+  const timeString = new Date(response?.created * 1000).toLocaleDateString();
 
-  try{
+  try {
     let event = stripe.webhooks.constructEvent(
-      payload , 
+      payload,
       sig!,
       process.env.STRIPE_WEBHOOK_SECRET!
-    )
+    );
 
-    console.log("event" , event.type)
+    // console.log("event" , event.type)
 
     // // here we write the logic after webhook catches the specified logic
 
-    // switch (event.type) {
-    //   case 'checkout.session.async_payment_failed':
-    //     const checkoutSessionAsyncPaymentFailed = event.data.object;
-    //     // Then define and call a function to handle the event checkout.session.async_payment_failed
-    //     break;
-    //   case 'checkout.session.async_payment_succeeded':
-    //     const checkoutSessionAsyncPaymentSucceeded = event.data.object;
-    //     // Then define and call a function to handle the event checkout.session.async_payment_succeeded
-    //     break;
-    //   case 'checkout.session.completed':
-    //     const checkoutSessionCompleted = event.data.object;
-    //     // Then define and call a function to handle the event checkout.session.completed
-    //     break;
-    //   case 'checkout.session.expired':
-    //     const checkoutSessionExpired = event.data.object;
-    //     // Then define and call a function to handle the event checkout.session.expired
-    //     break;
-    //   // ... handle other event types
-    //   default:
-    //     console.log(`Unhandled event type ${event.type}`);
-    // }
+    switch (event.type) {
+      // case 'checkout.session.async_payment_failed':
+      //   const checkoutSessionAsyncPaymentFailed = event.data.object;
+      //   // Then define and call a function to handle the event checkout.session.async_payment_failed
+      //   break;
+      case "checkout.session.async_payment_succeeded":
+        const checkoutSessionAsyncPaymentSucceeded = event.data.object;
+        const { customer_details } = checkoutSessionAsyncPaymentSucceeded;
+        const customerEmail = await customer_details?.email;
 
-    return NextResponse.json({status : 400 , event : event.type})
+        const mailRes = await fetch(
+          `${req.headers.get("origin")}/api/v1/checkout-sessions/send-email`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              send_to: await customerEmail,
+              subject: "寄付完了",
+              message:
+                "寄付にご協力いただきありがとうございます。寄付処理が完了したことをお知らせします",
+            }),
+          }
+        );
 
-    
+        return NextResponse.json(mailRes)
+
+        //   const checkoutSessionAsyncPaymentSucceeded = event.data.object;
+        //   // Then define and call a function to handle the event checkout.session.async_payment_succeeded
+        break;
+      // case "checkout.session.completed":
+        // Then define and call a function to handle the event checkout.session.completed
+        // break;
+      // case 'checkout.session.expired':
+      //   const checkoutSessionExpired = event.data.object;
+      //   // Then define and call a function to handle the event checkout.session.expired
+      //   break;
+      // // ... handle other event types
+      // default:
+      //   console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // return NextResponse.json({ status: 400, event: event.data.object });
   } catch (error) {
-    return NextResponse.json({status : 501 , error})
+    return NextResponse.json({ status: 501, error });
   }
-
 }
--- app/components/Email.tsx
+++ app/components/Email.tsx
@@ -15,3 +15,4 @@ export function Email({message} : Props) {
     </Html>
   );
 }
+
