af4d4855d26bf3394c7030f72bae09a14ef67fba
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-06-06 02:09:21 +0900

    test: send email

--- app/api-services/email.tsx
+++ app/api-services/email.tsx
@@ -10,41 +10,35 @@ type Props = {
 };
 
 export async function sendSuccesEmail({ recipient, subject, message }: Props) {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: 465, // メール送信用の固定ポート番号
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 465, // メール送信用の固定ポート番号
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const options = {
+    from: "info@me-net.or.jp",
+    to: "rajan.valencia@au.com",
+    subject: `[${process.env.ENV}] ${subject}`,
+    html: render(<Email message={message} />),
+  };
+
+  return await new Promise((resolve, reject) => {
+    transporter.sendMail(options, function (err) {
+      if (!err) {
+        resolve("Email sent!");
+      } else {
+        reject(err);
+      }
     });
-
-    const options = {
-      from: "info@me-net.or.jp",
-      to: "rajan.valencia@au.com",
-      subject: `[${process.env.ENV}] ${subject}`,
-      html: render(<Email message={message} />),
-    };
-
-    await new Promise((resolve, reject) => {
-      transporter.sendMail(options, function (err) {
-        if (!err) {
-          resolve("Email sent!");
-        } else {
-          reject(err);
-        }
-      });
-    });
-
-    return NextResponse.json({ status: 200, message: "Email sent" });
-  } catch (error) {
-    return NextResponse.json({ status: 500, error: "An error occurred" });
-  }
+  });
 }
 
 // TODO: sendErrorEmail
