7776ddd5e8e16b3de9d3af0e8561b6b4139a08ef
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-06-06 02:31:52 +0900

    test: send email

--- app/api-services/email.tsx
+++ app/api-services/email.tsx
@@ -9,17 +9,17 @@ type Props = {
   message: string;
 };
 
-export async function sendSuccesEmail({ recipient, subject, message }: Props) {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: 465, // メール送信用の固定ポート番号
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: 465, // メール送信用の固定ポート番号
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
+export async function sendSuccesEmail({ recipient, subject, message }: Props) {
   const options = {
     from: "info@me-net.or.jp",
     to: recipient,
@@ -27,16 +27,8 @@ export async function sendSuccesEmail({ recipient, subject, message }: Props) {
     html: render(<Email message={message} />),
   };
 
-  return await new Promise((resolve, reject) => {
-    transporter.sendMail(options, function (err) {
-      if (!err) {
-        resolve("Email sent!");
-      } else {
-        reject(err);
-      }
-    });
-  });
+  return await transporter.sendMail(options);
 }
 
 // TODO: sendErrorEmail
-// error credit番号でたたいて それで確認する、
+// error credit番号でたたいて それで確認する
