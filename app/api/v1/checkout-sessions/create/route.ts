7d9c50da2ce828e414ff23085846895dcb8bb422
Bishwas KOirala <wasubisu69@gmail.com>
2024-06-05 17:13:42 +0900

    fixed :: now it wont create new price id of a product with the same amount

--- app/api/v1/checkout-sessions/create/route.ts
+++ app/api/v1/checkout-sessions/create/route.ts
@@ -25,9 +25,11 @@ export const bodySchema = z.object({
 
 type BodyInterface = z.infer<typeof bodySchema>;
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string , {apiVersion : "2024-04-10"});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2024-04-10",
+});
 
-export async function POST(req: NextRequest , res : NextResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   const body: BodyInterface = await req.json();
   const validation = bodySchema.safeParse(body);
   if (!validation.success)
@@ -37,35 +39,60 @@ export async function POST(req: NextRequest , res : NextResponse) {
 
   // TODO: only create customer and price if it doesnt exist already
 
-  const {id : customerId} = await stripe.customers.create(customer);
+  const { id: customerId } = await stripe.customers.create(customer);
 
+  let priceId: string | undefined;
+  const existingPrice = await checPriceExistance(product_id, price);
+  if (typeof existingPrice != "string") {
+    const { id } = await stripe.prices.create({
+      product: product_id,
+      currency: "jpy",
+      // amount passed from the body
+      unit_amount: price,
+    });
+    priceId = id;
+  } else {
+    priceId = await existingPrice;
+  }
 
-  
-  const {id : priceId} = await stripe.prices.create({
-    product : product_id,
-    currency : "jpy",
-    // amount passed from the body
-    unit_amount : price
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: "embedded",
+    customer: customerId,
+    payment_method_types: ["card", "konbini"],
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+    automatic_tax: { enabled: false },
+    mode: "payment",
+    return_url: `${req.headers.get(
+      "origin"
+    )}/donationReturn?session_id={CHECKOUT_SESSION_ID}`,
   });
 
-
-  const session  = await stripe.checkout.sessions.create({
-    ui_mode : "embedded",
-    customer : customerId,
-    // TODO: customer のemail をretriveに帰るべき
-    payment_method_types : ["card" , "konbini" ],
-    line_items : [
-      {
-        price : priceId,
-        quantity : 1
-      }
-
-    ],
-    automatic_tax : {enabled : false},
-    mode : "payment",
-    return_url : `${req.headers.get('origin')}/donationReturn?session_id={CHECKOUT_SESSION_ID}`,
-  })
-  
-  return NextResponse.json({id : session.id , client_secret : session.client_secret});
-
+  return NextResponse.json({
+    id: session.id,
+    client_secret: session.client_secret,
+    priceId
+  });
 }
+
+const checPriceExistance = async (productId: string, amount: number) => {
+  const prices = await stripe.prices.list({
+    product: productId,
+  });
+
+  const matchingPrice = prices.data.find(
+    (price) => price.unit_amount === amount
+  );
+
+  if (matchingPrice) {
+    console.log("Price exists:", matchingPrice.id);
+    return matchingPrice.id;
+  } else {
+    console.log("Price not found for the given amount");
+    return undefined;
+  }
+};
